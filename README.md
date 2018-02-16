6da41195ff27d40f493a1b2240987165bd4d9c5a
sida <siningdream@gmail.com>
2018-02-16 21:10:23 +0800

    feature: initialize a google spreadsheet to store data

--- README.md
+++ README.md
@@ -1,7 +1,17 @@
 # What I Searched
 A chrome extension that help you record your search history
 
+## Known Issues
+
+#### Bad support for Baidu
+
+Baidu use Ajax or similar technology to send your input to their servers. But, this chrome extension only detect new searches when your browser loads new urls. As a result, only the first search each time you input into Baidu is recorded.
+
 ## References
 
 - [Chrome.storage.sync example](https://gist.github.com/IzumiSy/765cfd6dc02c79de875e)
 - [How to make a chrome extension](https://robots.thoughtbot.com/how-to-make-a-chrome-extension)
+- [chrome.identity User Authentication in a Chrome Extension](https://stackoverflow.com/questions/25044936/chrome-identity-user-authentication-in-a-chrome-extension)
+- [Getting the client's timezone in JavaScript](https://stackoverflow.com/a/34602679/6633748)
+- [OAuth 2.0 Scopes for Google APIs](https://developers.google.com/identity/protocols/googlescopes)
+- [Call An Asynchronous Javascript Function Synchronously](https://stackoverflow.com/a/33579720/6633748)
---
+++ init.js
@@ -0,0 +1,99 @@
+async function getSpreadsheetId() {
+    return await new Promise(function (resolve, reject) {
+        chrome.storage.sync.get({
+            spreadsheetId: false
+        }, (item) => {
+            if (item.spreadsheetId) {
+                resolve(item.spreadsheetId);
+            } else {
+                reject();
+            }
+        });
+    });
+}
+
+chrome.identity.getAuthToken({
+    interactive: true
+}, function (token) {
+    if (chrome.runtime.lastError) {
+        // TODO: notify popup.html
+        return;
+    }
+    getSpreadsheetId().then(function (spreadsheetId) {
+        alert(spreadsheetId);
+    }, function () {
+        let profilePromise = new Promise(function (resolve, reject) {
+            let xhr = new XMLHttpRequest();
+            xhr.open('GET', 'https://www.googleapis.com/oauth2/v2/userinfo?alt=json&access_token=' + token);
+            xhr.onload = function () {
+                resolve(JSON.parse(this.responseText).given_name);
+            };
+            xhr.send();
+        });
+
+        profilePromise.then((username) => {
+            let xhr = new XMLHttpRequest();
+            xhr.open('POST', 'https://sheets.googleapis.com/v4/spreadsheets');
+            xhr.setRequestHeader("Content-Type", "application/json");
+            xhr.setRequestHeader('Authorization', 'Bearer ' + token);
+            xhr.onload = function () {
+                //  TODO; store sheet id
+                let resp = JSON.parse(this.responseText);
+                chrome.storage.sync.set({
+                    username: username,
+                    spreadsheetId: resp.spreadsheetId,
+                    dataSheetId: resp.sheets[0].properties.sheetId
+                });
+            };
+            let initSheet = {
+                "properties": {
+                    "title": `${username}'s search history since ${(new Date()).toJSON().slice(0, 10)}`,
+                    "timeZone": Intl.DateTimeFormat().resolvedOptions().timeZone
+                },
+                "sheets": [{
+                    "properties": {
+                        "title": "Data"
+                    },
+                    "data": [{
+                        "startRow": 0,
+                        "startColumn": 0,
+                        "rowData": [{
+                            "values": [{
+                                    "userEnteredValue": {
+                                        "stringValue": "Date"
+                                    },
+                                    "userEnteredFormat": {
+                                        "textFormat": {
+                                            "bold": true
+                                        }
+                                    }
+                                },
+                                {
+                                    "userEnteredValue": {
+                                        "stringValue": "Count"
+                                    },
+                                    "userEnteredFormat": {
+                                        "textFormat": {
+                                            "bold": true
+                                        }
+                                    }
+                                },
+                                {
+                                    "userEnteredValue": {
+                                        "stringValue": "Content"
+                                    },
+                                    "userEnteredFormat": {
+                                        "textFormat": {
+                                            "bold": true
+                                        }
+                                    }
+                                }
+                            ]
+                        }]
+                    }]
+                }]
+            }
+            xhr.send(JSON.stringify(initSheet));
+        });
+    });
+});
