6be9df1b0d8c805f9ffcbaa98a3242bfb1741c88
sida <siningdream@gmail.com>
2018-02-16 21:11:28 +0800

    featrue: judge scope in content script to support google scholar search

--- url.js
+++ url.js
@@ -1,11 +1,18 @@
-let url = new URL(window.location.href);
-let params = url.searchParams;
-let keywords = params.get('q') || params.get('wd');
+(function () {
+    let url = new URL(window.location.href);
+    let regGoogle = /^\w*\.google\./;
+    let regBaidu = /^www\.baidu.com/;
+    if (!regGoogle.test(url.hostname) && !regBaidu.test(url.hostname)) {
+      return;
+    }
+    let params = url.searchParams;
+    let keywords = params.get('q') || params.get('wd');
 
-chrome.storage.sync.get({
-    'count': 0
-}, function (item) {
-    chrome.storage.sync.set({
-        'count': item.count + 1
-    })
-});
+    chrome.storage.sync.get({
+        'count': 0
+    }, function (item) {
+        chrome.storage.sync.set({
+            'count': item.count + 1
+        })
+    });
+})();
