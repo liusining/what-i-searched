25cd3941faee32d435991b7c63cae196670e117f
sida <siningdream@gmail.com>
2018-02-18 19:32:58 +0800

    refactor: user mvc and oo to organize code and add support to baidu

--- url.js
+++ url.js
@@ -1,18 +1,68 @@
 (function () {
-    let url = new URL(window.location.href);
-    let regGoogle = /^\w*\.google\./;
-    let regBaidu = /^www\.baidu.com/;
-    if (!regGoogle.test(url.hostname) && !regBaidu.test(url.hostname)) {
-      return;
-    }
-    let params = url.searchParams;
-    let keywords = params.get('q') || params.get('wd');
+    class Search {
+        constructor() {
+            this.url = new URL(window.location.href);
+            this.params = this.url.searchParams;
+        }
 
-    chrome.storage.sync.get({
-        'count': 0
-    }, function (item) {
-        chrome.storage.sync.set({
-            'count': item.count + 1
-        })
-    });
+        getKeywords() {
+            return this.params.get('q');
+        }
+    }
+
+    class BaiduSearch extends Search {
+        listen(callback) {
+            document.querySelector('#su').addEventListener('click', () => {
+                this.update();
+                callback();
+            });
+        }
+
+        update() {
+            this.url = new URL(window.location.href);
+            this.params = this.url.searchParams;
+        }
+
+        getKeywords() {
+            return document.querySelector('#kw').value;
+        }
+    }
+
+    let model = {
+        init: function (isBaidu) {
+            if (isBaidu) {
+                this.search = new BaiduSearch();
+            } else {
+                this.search = new Search();
+            }
+        },
+        incrementCount: function () {
+            chrome.storage.sync.get({
+                'count': 0
+            }, function (item) {
+                chrome.storage.sync.set({
+                    'count': item.count + 1
+                })
+            });
+        }
+    };
+
+    let controller = {
+        init: function () {
+            let hostname = (new URL(window.location.href)).hostname;
+            let regGoogle = /^\w*\.google\./;
+            let regBaidu = /^www\.baidu\.com/;
+            if (regGoogle.test(hostname)) {
+                model.init(false);
+            } else if (regBaidu.test(hostname)) {
+                model.init(true);
+                model.search.listen(model.incrementCount);
+            } else {
+                return;
+            }
+            model.incrementCount();
+        }
+    }
+
+    controller.init();
 })();
