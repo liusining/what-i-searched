8dab1544af7e0102be597bee2b138b930a7d1776
liusining <siningdream@gmail.com>
2019-01-11 22:12:26 +0800

    Feature: connect dynamodb

---
+++ backend/mydb/go.mod
@@ -0,0 +1,3 @@
+module github.com/liusining/what-i-searched/backend/mydb
+
+require github.com/aws/aws-sdk-go v1.16.17
---
+++ backend/mydb/mydb.go
@@ -0,0 +1,70 @@
+package mydb
+
+import (
+	"fmt"
+	"strconv"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/sns"
+)
+
+const (
+	tableName = "what_i_searched"
+)
+
+var awsSns *sns.SNS
+
+func init() {
+	awsSession := session.Must(session.NewSession(&aws.Config{
+		Region: aws.String("ap-northeast-1")}))
+	awsSns = sns.New(awsSession)
+}
+
+// NewDB generate a new dynamodb client
+func NewDB() *dynamodb.DynamoDB {
+	awsSession := session.Must(session.NewSession(&aws.Config{
+		Region: aws.String("ap-northeast-1")}))
+	db := dynamodb.New(awsSession)
+	return db
+}
+
+// CreateRecord upload a search record to DB
+func CreateRecord(db *dynamodb.DynamoDB, timestamp, keywords string) error {
+	u, err := strconv.ParseInt(timestamp[:len(timestamp)-3], 10, 64)
+	if err != nil {
+		return fmt.Errorf("parse timestamp: %s", err)
+	}
+	zone := time.FixedZone("Asia/Beijing", 8*3600)
+	uTime := time.Unix(u, 0).In(zone)
+	humanDate := uTime.Format("2006-01-02")
+	humanTime := uTime.Format("15:04:05 -0700")
+	put := &dynamodb.PutItemInput{
+		TableName: aws.String(tableName),
+		Item: map[string]*dynamodb.AttributeValue{
+			"Timestamp": {N: aws.String(timestamp)},
+			"Date":      {S: aws.String(humanDate)},
+			"Time":      {S: aws.String(humanTime)},
+			"Keywords":  {S: aws.String(keywords)},
+		},
+	}
+	_, err = db.PutItem(put)
+	if err != nil {
+		return err
+		// log.Printf("upload search record: %s\n", err)
+		// input := &sns.PublishInput{
+		// 	Message:  aws.String(fmt.Sprintf("upload search record: %s", err)),
+		// 	TopicArn: aws.String("arn:aws:sns:ap-northeast-1:129453598127:TellMeSomething"),
+		// 	Subject:  aws.String("What I Searched Error"),
+		// }
+		// _, e := awsSns.Publish(input)
+		// if e != nil {
+		// 	log.Printf("send sns msg: %s\n", e)
+		// 	return err
+		// }
+		// return err
+	}
+	return nil
+}
