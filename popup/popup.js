b82033f067ed642cf2c005bfb30955417040cf7f
liusining <siningdream@gmail.com>
2019-01-13 23:57:56 +0800

    Feature: display searches count in the popup

--- popup/popup.js
+++ popup/popup.js
@@ -1,13 +1,29 @@
 let username = document.getElementById('username');
 chrome.storage.sync.get({
-    username: ''
+  username: 'Missing Name',
+  authString: '',
+  createRecordURL: '',
+  getCountURL: ''
 }, function (item) {
-    username.innerText = item.username;
-});
-
-let data = document.getElementById('data');
-chrome.storage.sync.get({
-    "count": 0
-}, function (item) {
-    data.innerText = item.count;
+  // test whether username has been initialized
+  if (item.username === "Missing Name") {
+    username.innerHTML = "cannot find your name";
+  } else {
+    username.innerHTML = item.username;
+  }
+  // test whether database connection urls have been set
+  if (item.getCountURL === "" || item.createRecordURL === "") {
+    let msgContainer = document.getElementsByClassName('message')[0];
+    msgContainer.innerHTML = `<p>You haven't configured database connection yet.</p>
+    <p>Right Click the icon and go to options page to set configuration.</p>`
+  } else {
+    let countContainer = document.getElementById('data');
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", item.getCountURL, true);
+    xhr.setRequestHeader('Authorization', 'Basic ' + item.authString);
+    xhr.onload = function() {
+      countContainer.innerHTML = xhr.responseText;
+    }
+    xhr.send();
+  }
 });
